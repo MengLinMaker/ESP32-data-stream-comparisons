4589d1e70ac66d88f2f36deb7125e4f57301749d
MengLinCoding <39476147+MengLinCoding@users.noreply.github.com>
2022-04-18 03:48:21 +1000

    BNO080 data streaming modified library

---
+++ I2C_stream/BNO080/Bno080_data_stream/Bno080_data_stream.ino
@@ -0,0 +1,79 @@
+/*
+  Name: BNO080 USB UART data streaming
+  Purpose: Serial printing BNO080 IMU quaterions,acceleration and angular velocity on Esp32 at high frequencies.
+  Credits: Modified BNO080 library based on Nathan Seidle: https://github.com/sparkfun/SparkFun_BNO080_Arduino_Library
+  @author Meng Lin
+  @version 1.0 18/04/22
+*/
+
+#include "src/TinyBNO080.h"
+#include "src/IMUData.h"
+//#include "SparkFun_BNO080_Arduino_Library.h"
+
+BNO080 IMU_BNO080;
+IMUdata IMU;
+
+// Change data rate
+const long frequency = 100;
+const long ideal_period = round(40*1000000.0/frequency);
+static SemaphoreHandle_t timer_sem;
+
+
+
+
+
+void sample_timer_task(void *param)
+{
+  timer_sem = xSemaphoreCreateBinary();
+  while (1) {
+    xSemaphoreTake(timer_sem, portMAX_DELAY);
+    
+    //Serial.println(timeStep());
+    IMU.update_BNO080(&IMU_BNO080, 5000);
+    IMU.printAll();
+  }
+}
+
+void IRAM_ATTR onTimer(){
+  static BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  xSemaphoreGiveFromISR(timer_sem, &xHigherPriorityTaskWoken);
+  if( xHigherPriorityTaskWoken) {
+    portYIELD_FROM_ISR(); // this wakes up sample_timer_task immediately
+  }
+}
+
+
+
+
+
+void setup(){
+  Serial.begin(2000000);
+  
+  // Define I2C_SDA and SCL pins
+  Wire.begin(21, 22);
+  Wire.setClock(400000);
+
+  const unsigned char updateTime = 5;
+  IMU_BNO080.begin();
+  IMU_BNO080.enableRotationVector(updateTime);
+  IMU_BNO080.enableAccelerometer(updateTime);
+  IMU_BNO080.enableGyro(updateTime);
+
+  // Create timer interrupt
+  hw_timer_t *ticker = NULL;
+  ticker = timerBegin(0, 2, true);
+  timerAttachInterrupt(ticker, onTimer, true);
+  timerAlarmWrite(ticker, ideal_period, true);
+  timerAlarmEnable(ticker);
+
+  xTaskCreatePinnedToCore(sample_timer_task, "sample_timer", 4096, NULL, configMAX_PRIORITIES - 1, NULL, 1);
+}
+
+void loop(){
+  /*
+  Serial.println("\n\n");
+  for(int i=0; i < 100; i++){Serial.println("Hello, my name is Bob, from Bob the builder!");}
+  Serial.println("\n\n");
+  vTaskDelay(50);
+  //*/
+}
